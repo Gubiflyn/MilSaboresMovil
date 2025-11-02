bbf12a6cfea2dd5735b4c34fc59f1a619b363938
Gubier Zamora <gub.zamora@duocuc.cl>
2025-11-02 11:07:13 -0300

    Agregue mi perfil arregle un poco el homesadas

--- app/src/main/java/com/example/proyectologin005d/ui/theme/Type.kt
+++ app/src/main/java/com/example/proyectologin005d/ui/theme/Type.kt
@@ -28,7 +28,7 @@ val Typography = Typography(
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.5.spsdadasdas
     )
     */
 )
